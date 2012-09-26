994d429a47c2fa8d06c9b10c0e7920633585a274
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-09-26 03:03:18 -0400

    Minor edit

--- Ptr.h
+++ Ptr.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#ifndef RefCount_Ptr_h
-#define RefCount_Ptr_h
+#ifndef PTR_INC
+#define PTR_INC
 #define T ptr
 
 typedef struct T *T;
@@ -15,24 +15,24 @@ typedef struct T *T;
 //
 // Return a reference-counted pointer 
 // initialized to point at p.
-ptr new(void *p);
+T new(void *p);
 
 // Increase the reference count
 // of p and return a pointer to p.
-ptr cp(ptr p);
+T cp(T p);
 
 // Decrease the reference count
 // of p. If the reference count
 // has reached 0, free the contained
 // pointer
-void del(ptr p);
+void del(T p);
 
 // Same as del, except that *f
 // is called on the contained pointer
 // before it is freed
-void delFunc(ptr p, void (*f)(void*));
+void delFunc(T p, void (*f)(void*));
 
 // Return the contained pointer.
-void* Ptr(ptr p);
+void* Ptr(T p);
 
 #endif
